3ce9980b67a2ddf15e18702018ec78e5f787c1aa
hblaxton <122068882+hblaxton@users.noreply.github.com>
2023-02-09 13:52:26 -0500

    Update HelloWorld.vue

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,6 +1,32 @@
 <template>
   <h1>{{ msg }}</h1>
 
+<button class="duplicate" v-on:click="duplicate">duplicate</button>
+<button class="button" v-on:click="color">button</button>
+<button class="modifytitle" v-on:click="name">modifytitle</button>
+<button class="button" v-on:click="deletes">delete me</button>
+<button data-toggle-btn v-on:click="toggle">toggle description</button>
+
+<div class="wrapper">
+  <div class="container">
+  <img class="image" src="https://upload.wikimedia.org/wikipedia/commons/e/eb/Ash_Tree_-_geograph.org.uk_-_590710.jpg">
+  <div class="header">
+    <h3>Landscaping Company</h3>
+    <h4>A beautiful green tree</h4>
+  </div>
+</div>
+  <details class="details">
+    <summary>Description</summary>
+    <div>
+      <ul>
+        <li>A tree as beautiful as this one should be cut down</li>
+        <li>Trees!</li>
+       </ul>
+    </div>
+    </details>
+</div>
+
+
   <p>
     <a href="https://vitejs.dev/guide/features.html" target="_blank">
       Vite Documentation
@@ -26,8 +52,70 @@ defineProps({
 const state = reactive({ count: 0 })
 </script>
 
+<script>
+const container = document.querySelector(".container");
+const bg = document.querySelector("#bg");
+const details = document.querySelector('summary');
+
+export default {
+methods: {
+toggle(){
+document.querySelector('button[data-toggle-btn]').addEventListener('click', (e) => {
+  if (details.parentNode.getAttribute('open')) {
+    details.parentNode.removeAttribute('open');
+  }
+  else {
+    details.parentNode.setAttribute('open','open');    
+  }
+});
+},
+
+// duplicate the 1st item
+duplicate(){
+document.querySelector('.duplicate').addEventListener('click', function(e) {
+  const itemToClone = document.querySelector('.wrapper').cloneNode(true);
+  document.body.appendChild(itemToClone);
+});
+},
+
+color(){
+bg.addEventListener("click", (e) => {
+  const colors = ["red", "orange", "yellow", "green", "blue", "purple"];
+  const randomColor = colors[Math.floor(Math.random() * colors.length)];
+  container.style.backgroundColor = randomColor;
+});
+},
+ 
+
+name(){
+document.querySelector('.modifytitle').addEventListener('click', function(e) {
+  let name = prompt("Rename the company");
+  if (name) {
+    document.querySelector('.wrapper h3').innerText = name;
+  }
+});
+},
+
+deletes(){
+document.querySelector('#deletelastcard').addEventListener('click', function(e) {
+  let wantsTo = confirm("Are you so sure?");
+  if (wantsTo) {
+    if (document.querySelector('.wrapper:last-child') !== document.querySelector('.wrapper')) {
+      document.querySelector('.wrapper:last-child').remove();      
+    }
+    else {
+      alert("YOU CAN NOT DELETE OUR TREE");
+    }
+  }
+});
+},
+}
+}
+</script>
+
 <style scoped>
 a {
   color: #42b983;
 }
 </style>
+
